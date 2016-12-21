4ca1661eefe72ae8935c2301ee90807ddd5f7326
Taniadz <tdziubenko@ukr.net>
2016-12-21 13:29:14 +0200

    2.4

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
 application = get_wsgi_application()
