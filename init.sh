aec461eea18818d0f12ce9847c9a1048ff889d7b
Taniadz <tdziubenko@ukr.net>
2016-12-21 14:57:22 +0200

    2.12

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
 
 sudo ln -sf /home/box/web/etc/django.conf /etc/gunicorn.d/ask
