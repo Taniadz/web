3621177ef4b1bd6c80644864d3c7a0960da18385
Taniadz <tdziubenko@ukr.net>
2016-12-21 15:43:38 +0200

    2.17

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -6,13 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
 import os
-
-from django.core.wsgi import get_wsgi_application
-
-import sys
-sys.path.append('/home/box/web/ask')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
