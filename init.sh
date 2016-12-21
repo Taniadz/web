1658459b6a923b59560c5e990971183123b1b21b
Taniadz <tdziubenko@ukr.net>
2016-12-21 13:06:34 +0200

    2.2

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
 
+sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/wsgi
+
 gunicorn -c /home/box/web/etc/django.py wsgi
 sudo /etc/init.d/gunicorn restart
