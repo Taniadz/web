20c2ed2928a5dafa0239be5730cce5bf9a4c7659
Taniadz <tdziubenko@ukr.net>
2016-12-21 13:26:42 +0200

    2.3

--- etc/django.py
+++ etc/django.py
@@ -1,2 +1,10 @@
-pythonpath = '/home/box/web/ask/ask'
-bind = "0.0.0.0:8000"
+CONFIG = {
+    'mode': 'django',
+    'working_dir': '/home/box/web/ask/ask',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=4',
+        '--timeout=60',
+        'ask.wsgi'
+    )
+}
