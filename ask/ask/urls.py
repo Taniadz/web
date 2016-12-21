ce2358a9ccab659568ab5ce8d546b904dcccf215
Taniadz <tdziubenko@ukr.net>
2016-12-21 16:19:09 +0200

    2.20

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,6 +19,7 @@ from qa import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.test),
     url(r'^login/', views.test),
     url(r'^signup/', views.test),
     url(r'^question/(?P<id>\d+)', views.test),
